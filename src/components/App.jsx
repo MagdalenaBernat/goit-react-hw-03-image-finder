3bc0948651dfee24cb8c5d79fdc560fae3ad296b
MagdalenaBernat <magdalena.bernat@onet.pl>
2024-07-13 22:05:45 +0200

    components create

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import React from "react";
+import Searchbar from "./Searchbar/Searchbar"
+import ImagdGallery from "./ImageGallery/ImageGallery"
+import ImageGalleryItem from "./ImageGalleryItem/ImageGalleryItem"
+import Button from "./Button/Button"
+import Loader from "./Loader"
+import Modal from "./Modal/Modal"
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Searchbar />
+      <ImagdGallery />
+      <ImageGalleryItem />
+      <Button />
+      <Loader />
+      <Modal />   
+    </>
   );
 };
---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,7 @@
+export const ImageGalleryItem = () => {
+    return (
+        <>
+
+        </>
+    )
+}
